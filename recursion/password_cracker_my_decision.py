6682bb667d121ffd76d2a9d301e01eeda856a458
timur <timurka.zavyalov@mail.ru>
2023-01-24 02:41:42 +0500

    git init

--- recursion/password_cracker_my_decision.py
+++ recursion/password_cracker_my_decision.py
@@ -38,3 +38,4 @@ if __name__ == '__main__':
         login_attempt: str = "ophetushtrinyaiqoxrysqleliwfnytwrlgxmophetushtrwfnytwinyaiqykaxonophetushtr"
         result = password_cracker(passwords, login_attempt)
         print(result)
+
